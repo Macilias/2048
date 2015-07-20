93fb801979c3f629b565bd7cd68ac688a0ca5886
mac <maciekn@gmx.de>
2015-07-20 20:14:11 +0200

    init 1 hello world

---
+++ src/main/java/com/macilias/games/view/Main.java
@@ -0,0 +1,12 @@
+package com.macilias.games.view;
+
+/**
+ * Created by mac on 20.07.15.
+ */
+public class Main {
+
+
+    public static void main (String... args) {
+        System.out.println("Hello World");
+    }
+}
